df38686ed41a58596ce1a995614208bdf8232cf0
KAKALZF <575800503@qq.com>
2019-07-05 17:15:36 +0800

    zuul负载均衡未实现

--- platform-hub/src/main/java/com/kd100/cloudplatform/platformhub/PlatformHubApplication.java
+++ platform-hub/src/main/java/com/kd100/cloudplatform/platformhub/PlatformHubApplication.java
@@ -2,12 +2,15 @@ package com.kd100.cloudplatform.platformhub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
+@SpringCloudApplication
 @EnableEurekaClient
 @EnableFeignClients //1
+@EnableZuulProxy
 public class PlatformHubApplication {
 	public static void main(String[] args) {
         SpringApplication.run(PlatformHubApplication.class, args);
